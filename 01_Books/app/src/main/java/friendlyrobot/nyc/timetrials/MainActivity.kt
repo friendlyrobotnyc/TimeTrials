aaa92ef0210633410ae89f8608192a738c8733bc
Brian Plummer <brian.r.plummer@gmail.com>
2019-06-29 22:45:31 -0400

    hands down pencils up

--- 01_Books/app/src/main/java/friendlyrobot/nyc/timetrials/MainActivity.kt
+++ 01_Books/app/src/main/java/friendlyrobot/nyc/timetrials/MainActivity.kt
@@ -2,11 +2,65 @@ package friendlyrobot.nyc.timetrials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.moshi.Moshi
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.view_book.view.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var myadapter : MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myadapter = MyAdapter()
+        mylist.layoutManager = LinearLayoutManager(this)
+        mylist.adapter = myadapter
+
+
+        val moshi = Moshi.Builder().build()
+        val bookSearchResponse = moshi.adapter<BookSearchResponse>(BookSearchResponse::class.java).fromJson(silly)
+
+        val listOfBooks = mutableListOf<Book>()
+        bookSearchResponse?.docs?.forEach { listOfBooks.add(it.toBook()) }
+        myadapter.clearAndAdd(listOfBooks)
+    }
+}
+
+class MyAdapter : RecyclerView.Adapter<BookDocumentVH>() {
+
+    private val books:MutableList<Book> = mutableListOf<Book>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookDocumentVH {
+
+        val myview = LayoutInflater.from(parent.context).inflate(R.layout.view_book, parent, false)
+
+        return BookDocumentVH(myview)
+    }
+
+    override fun getItemCount() = books.size
+
+    override fun onBindViewHolder(holder: BookDocumentVH, position: Int) {
+        holder.bind(books[position])
+    }
+
+    fun clearAndAdd(booksToAdd: List<Book>) {
+        books.clear()
+        books.addAll(booksToAdd)
+        notifyDataSetChanged()
+    }
+}
+
+class BookDocumentVH(bookDoc : View) : RecyclerView.ViewHolder(bookDoc) {
+
+    fun bind(book: Book) {
+        itemView.book_title.text = book.title
     }
 }
--- 01_Books/app/src/test/java/friendlyrobot/nyc/timetrials/ExampleUnitTest.kt
+++ 01_Books/app/src/test/java/friendlyrobot/nyc/timetrials/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package friendlyrobot.nyc.timetrials
 
+import com.squareup.moshi.Moshi
+import okio.Okio
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.InputStream
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,23 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun BookSearchResponse() {
+
+        val myStream = inputStream("/search_response.json")
+        assertNotNull(myStream)
+
+        val moshi = Moshi.Builder().build()
+        val booksearchResponse = moshi.adapter<BookSearchResponse>(BookSearchResponse::class.java)
+            .fromJson(Okio.buffer(Okio.source(myStream)))
+
+        assertNotNull(booksearchResponse)
+        assertEquals(booksearchResponse!!.num_found, 336)
     }
+
+    private fun inputStream(path: String): InputStream {
+        return ExampleUnitTest::class.java!!.getResourceAsStream(path)
+    }
+
 }
