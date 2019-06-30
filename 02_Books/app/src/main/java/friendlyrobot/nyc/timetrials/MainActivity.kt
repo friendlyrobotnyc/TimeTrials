b47aa3a4eb63638f32e32a73524f988f4468bea1
Brian Plummer <brian.r.plummer@gmail.com>
2019-06-30 16:21:00 -0400

    basic rv with a faked out list

--- 02_Books/app/src/main/java/friendlyrobot/nyc/timetrials/MainActivity.kt
+++ 02_Books/app/src/main/java/friendlyrobot/nyc/timetrials/MainActivity.kt
@@ -2,23 +2,67 @@ package friendlyrobot.nyc.timetrials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.squareup.moshi.JsonClass
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.moshi.Moshi
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_book.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bookAdapter: BookAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        bookAdapter = BookAdapter()
+        recyclerView.adapter = bookAdapter
+
+        val moshi = Moshi.Builder().build()
+        val smallResponse = moshi.adapter<ApiBookResponse>(ApiBookResponse::class.java).fromJson(simpleSearchResponse)
+
+        for (i in 1..10) {
+            smallResponse?.docs?.forEach {
+                bookAdapter.add(it.toBook())
+            }
+        }
+        bookAdapter.notifyDataSetChanged()
     }
 }
 
-@JsonClass(generateAdapter = true)
-data class ApiBookResponse(
-        val num_found : Int,
-        val docs : Array<ApiDocument>
-)
 
-@JsonClass(generateAdapter = true)
-data class ApiDocument(
-        val title : String
-)
+class BookAdapter : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+
+    private val books = mutableListOf<Book>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val holder = BookViewHolder(inflater.inflate(R.layout.item_book, parent, false))
+            return holder
+    }
+
+    override fun getItemCount() = books.size
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        books.getOrNull(position)?.let { holder.bind(it) }
+    }
+
+    class BookViewHolder(bookView: View) : RecyclerView.ViewHolder(bookView) {
+        fun bind(book: Book) {
+            itemView.book_title.text = book.title
+        }
+    }
+
+    fun add(book: Book) {
+        books.add(book)
+    }
+}
+
+
+
+
--- 02_Books/app/src/test/java/friendlyrobot/nyc/timetrials/ModelTest.kt
+++ 02_Books/app/src/test/java/friendlyrobot/nyc/timetrials/ModelTest.kt
@@ -6,17 +6,12 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class ModelTest {
 
     @Test
     fun `Search Book response test `() {
 
-        val myStream = ExampleUnitTest::class.java.getResourceAsStream("/search_resp.json")
+        val myStream = ModelTest::class.java.getResourceAsStream("/search_resp.json")
         assertNotNull(myStream)
 
         val moshi = Moshi.Builder().build()
